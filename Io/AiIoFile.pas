9b771300570a1bcda759a523c40eb4da5cc67527
Prof1983 <prof1983@yandex.ru>
2012-06-01 12:51:52 +0600

    Added Io form AReason/Ai

---
+++ Io/AiIoFile.pas
@@ -0,0 +1,349 @@
+﻿{**
+@Abstract(Io)
+@Author(Prof1983 prof1983@ya.ru)
+@Created(03.04.2005)
+@LastMod(15.05.2012)
+@Version(0.5)
+
+0.0.0.6 - 03.04.2005
+0.0.0.5 - 17.03.2005 - TFileProf
+0.0.0.4 - 31.05.2004 -
+0.0.0.3 - 19.05.2004 -
+0.0.0.2 - 27.01.2004 - Ident=2 - архив
+0.0.0.1 - 09.01.2004 - Сознан тип: TFileProfHeader
+}
+unit AiIoFile;
+
+interface
+
+uses
+  Classes,
+  ABaseUtils3, AIoTypes, AStreamObj, ATypes;
+
+type
+  UInt032 = UInt32;
+
+type
+  TFileProfInfo = record
+    Prof: array[0..3] of Char; {='Prof'}
+    Ident: UInt032;            {Идентификатор - тип файла}
+    Version1: UInt032;         {Версия типа файла}
+    Version2: UInt032;         {Версия файла}
+    IdentS: String;
+    Version1S: String;
+    Version2S: String;
+  end;
+
+  (*TFileProfHeader = packed record {16}
+    Prof: array[0..3] of Char; {='Prof'}
+    Ident: UInt032;            {Идентификатор - тип файла}
+    Version1: UInt032;         {Версия типа файла}
+    Version2: UInt032;         {Версия файла}
+  end;*)
+
+  TFileProfHeader = packed record {16}
+    Prof: array[0..3] of Byte; {=Char'Prof'}
+    Ident: UInt32;             {Идентификатор - тип файла}
+    Version1: UInt32;          {Версия типа файла}
+    Version2: UInt32;          {Версия файла}
+  end;
+
+  {Ident
+   1 - база данных
+     Version1
+     1 - свободная структура
+       Version2
+       1 - БД для ИИ (AIFreim, AIFreimA) (БЗ)
+       2 - Дополнительный файл для БЗ
+       3 - БД для ИИ (v3.0)
+     2 - БД для ИИ (v4.0)
+       Version2
+       1 - HProf, HFileAI, Блоки фреймов, Дополнительные блоки. Блоки по 4096 байт.
+       2 - HProf, HFileAI, Блоки FAT(1024 блоков), Блоки фреймов, Дополнительные блоки. Блоки по 4096 байт. v. 0.0.2
+       3 - то-же, HProf3 v. 0.0.3
+       4 - HProf, HFileProfAIRecF, Записи фреймов TAIFreimRecF начиная с 0
+   2 - архив
+     Version1
+     1 -
+   'text' - текстовый файл
+     Version1
+     '_dos' - текстовый файл в колировке DOS 866. Переносы строки $0A$0D
+     '_win' - текстовый файл в кодировке Windows 1251. Переносы строки $0A$0D
+     'unix' - текстовый файл с переносами строки #0.
+  }
+
+  TFileProf = class(TProfFile)
+  private
+    FHeader: TFileProfHeader;
+  public
+    function HeaderSet(var Header: TFileProfHeader; Ident, Version1,
+      Version2: UInt32): TError;
+    function HeaderWrite(const Header: TFileProfHeader): TError;
+    function HeaderRead(var Header: TFileProfHeader): TError;
+    function Open(FileName: String; Mode: TProfFileOpenMode): Boolean; override;
+    function OpenCreate(FileName: String; Header: TFileProfHeader): TError;
+  public
+    property Header: TFileProfHeader read FHeader;
+  end;
+
+procedure ArrayByteToHeader(
+  const A: TArrayByte;
+  var Header: TFileProfHeader
+  );
+
+(*procedure IOFileProfClose( {Закрывает prof-файл}
+  HFile: THandle             {in}{Идентификатор файла}
+  );
+
+function IOFileProfCreate(
+  FileName: String;
+  Header: TFileProfHeader
+  ): THandle;
+
+procedure IOFileProfInfoGet( {Возвращает инфармацию о prof-файле}
+  HFile: THandle;            {in}{Идентификатор файла}
+  var Info: TFileProfInfo       {out}{Информация}
+  );
+
+function IOFileProfOpen( {Открывает prof-файл и проверяет тип}
+  FileName: String;             {in}{Имя файла}
+  Ident: UInt032 = 0            {in}{Тип файла}
+  ): THandle;                {Идентификатор}*)
+
+procedure HeaderClear(
+  var Header: TFileProfHeader
+  );
+
+procedure HeaderToArrayByte(
+  var A: TArrayByte;
+  Header: TFileProfHeader
+  );
+
+implementation
+
+procedure ArrayByteToHeader(const A: TArrayByte; var Header: TFileProfHeader);
+begin
+  Header.Prof[0] := A[0];
+  Header.Prof[1] := A[1];
+  Header.Prof[2] := A[2];
+  Header.Prof[3] := A[3];
+  Header.Ident := cArrayByteToUInt32(ArrayByteCopy(A, 4, 4));
+  Header.Version1 := cArrayByteToUInt32(ArrayByteCopy(A, 8, 4));
+  Header.Version2 := cArrayByteToUInt32(ArrayByteCopy(A, 12, 4));
+end;
+
+procedure HeaderClear(var Header: TFileProfHeader);
+begin
+  Header.Prof[0] := 0;
+  Header.Prof[1] := 0;
+  Header.Prof[2] := 0;
+  Header.Prof[3] := 0;
+  Header.Ident := 0;
+  Header.Version1 := 0;
+  Header.Version2 := 0;
+end;
+
+procedure HeaderToArrayByte(var A: TArrayByte; Header: TFileProfHeader);
+begin
+  SetLength(A, 16);
+  A[0] := Header.Prof[0];
+end;
+
+(*var
+  Files: array of record
+    F: THandle;
+    H: TFileProfHeader;
+  end;
+
+{Procedures and Functions}
+
+function _FreeGet: UInt032;
+var
+  I: Int032;
+begin
+  for I := 0 to High(Files) do if Files[I].F = 0 then begin Result := I; Exit; end;
+  Result := Length(Files); SetLength(Files, Result+1);
+end;
+
+function _IndGet(HFile: THandle): Int032;
+var
+  I: Int032;
+begin
+  Result := -1;
+  for I := 0 to High(Files) do if Files[I].F = HFile then begin Result := I; Exit; end;
+end;
+
+procedure _FileProfClose(I: UInt032);
+begin
+  ioFileProfClose(Files[I].F);
+  Files[I].F := 0;
+end;
+
+function _FileProfIdentToStr(Ident: UInt032): String;
+begin
+  case Ident of
+    1: Result := 'База данных';
+    2: Result := 'Архив';
+  else
+    Result := 'Uncnown';
+  end;
+end;
+
+function _FileProfVersion1ToStr(Ident: UInt032; Version1: UInt032): String;
+begin
+  Result := '';
+end;
+
+function _FileProfVersion2ToStr(Ident: UInt032; Version1, Version2: UInt032): String;
+begin
+  Result := '';
+end;
+
+procedure IOFileProfClose(HFile: THandle);
+var
+  I: Int032;
+begin
+  I := _IndGet(HFile);
+  if I < 0 then begin
+    IOFileClose(HFile);
+    Exit;
+  end;
+  _FileProfClose(I);
+end;
+
+function IOFileProfCreate(FileName: String; Header: TFileProfHeader): THandle;
+begin
+  Result := IOFileCreate(FileName);
+  if Result = 0 then Exit;
+  if IOFileWrite(Result, Header, SizeOf(Header)) <> SizeOf(Header) then begin
+    IOFileClose(Result);
+    Result := 0;
+    Exit;
+  end;
+end;
+
+procedure IOFileProfInfoGet(HFile: THandle; var Info: TFileProfInfo);
+var
+  I: Int032;
+begin
+  I := _IndGet(HFile);
+  if I < 0 then Exit;
+  Info.Prof[0] := Files[I].H.Prof[0];
+  Info.Prof[1] := Files[I].H.Prof[1];
+  Info.Prof[2] := Files[I].H.Prof[2];
+  Info.Prof[3] := Files[I].H.Prof[3];
+  Info.Ident := Files[I].H.Ident;
+  Info.Version1 := Files[I].H.Version1;
+  Info.Version2 := Files[I].H.Version2;
+  Info.IdentS := _FileProfIdentToStr(Info.Ident);
+  Info.Version1S := _FileProfVersion1ToStr(Info.Ident, Info.Version1);
+  Info.Version2S := _FileProfVersion2ToStr(Info.Ident, Info.Version1, Info.Version2);
+end;
+
+function IOFileProfOpen(FileName: String; Ident: UInt032 = 0): THandle;
+var
+  I: Int032;
+begin
+  Result := IOFileOpen(FileName, fmOpenReadWrite, False);
+  if Result = 0 then Exit;
+  I := _FreeGet;
+  Files[I].F := Result;
+  ioFileRead(Result, Files[I].H, SizeOf(Files[I].H));
+  if (Files[I].H.Prof <> 'Prof') or ((Ident > 0) and (Files[I].H.Ident <> Ident)) then begin
+    _FileProfClose(I);
+    Result := 0;
+  end;
+end;*)
+
+{TFileProf}
+
+function TFileProf.HeaderRead(var Header: TFileProfHeader): TError;
+var
+  B: UInt08;
+  Stream: TProfStream;
+begin
+  Result := 1;
+  HeaderClear(Header);
+  Stream := GetStream;
+  if not(Assigned(Stream)) then Exit;
+  Seek(0);
+  Stream.ReadUInt08(B); Header.Prof[0] := B;
+  Stream.ReadUInt08(B); Header.Prof[1] := B;
+  Stream.ReadUInt08(B); Header.Prof[2] := B;
+  Stream.ReadUInt08(B); Header.Prof[3] := B;
+  Stream.ReadUInt32(Header.Ident);
+  Stream.ReadUInt32(Header.Version1);
+  Stream.ReadUInt32(Header.Version2);
+  Result := 0;
+end;
+
+function TFileProf.HeaderSet(var Header: TFileProfHeader; Ident, Version1,
+  Version2: UInt32): TError;
+begin
+  {Result := 1;}
+  HeaderClear(Header);
+  Header.Prof[0] := cCharToByte('P');
+  Header.Prof[1] := cCharToByte('r');
+  Header.Prof[2] := cCharToByte('o');
+  Header.Prof[3] := cCharToByte('f');
+  {Header.Ident := 1;
+  Header.Version1 := 2;
+  Header.Version2 := 4;}
+  Header.Ident := Ident;
+  Header.Version1 := Version1;
+  Header.Version2 := Version2;
+  Result := 0;
+end;
+
+function TFileProf.HeaderWrite(const Header: TFileProfHeader): TError;
+{var
+  Stream: TFileStream;}
+begin
+  Result := 1;
+  {Stream := GetStream;
+  if not(Assigned(Stream)) then Exit;
+  Seek(0);
+  Stream.Write(Header.Prof, SizeOf(Header.Prof));
+  Stream.Write(Header.Ident, SizeOf(Header.Ident));
+  Stream.Write(Header.Version1, SizeOf(Header.Version1));
+  Stream.Write(Header.Version2, SizeOf(Header.Version2));
+  Result := 0;}
+end;
+
+function TFileProf.Open(FileName: String; Mode: TProfFileOpenMode): Boolean;
+begin
+  Result := inherited Open(FileName, Mode);
+  if not(Result) then Exit;
+  if HeaderRead(FHeader) <> 0 then
+  begin
+    Close();
+    Result := False;
+    Exit;
+  end;
+  if (FHeader.Prof[0] <> cCharToByte('P')) or
+    (FHeader.Prof[1] <> cCharToByte('r')) or
+    (FHeader.Prof[2] <> cCharToByte('o')) or
+    (FHeader.Prof[3] <> cCharToByte('f')) then
+  begin
+    Close();
+    Result := False;
+    Exit;
+  end;
+  Result := True;
+end;
+
+function TFileProf.OpenCreate(FileName: String; Header: TFileProfHeader): TError;
+begin
+  FHeader := Header;
+  Result := inherited OpenCreate(FileName);
+  if Result <> 0 then Exit;
+  if HeaderWrite(FHeader) <> 0 then
+  begin
+    Result := 1;
+    Close;
+    Exit;
+  end;
+end;
+
+end.
+
---
+++ Io/AiIoFileKb.pas
@@ -0,0 +1,491 @@
+﻿{**
+@Abstract(БЗ фреймов)
+@Author(Prof1983 prof1983@ya.ru)
+@Created(17.03.2005)
+@LastMod(16.05.2012)
+@Version(0.5)
+
+0.0.0.4 - 14.02.2006 - Coments
+0.0.0.3 - 15.12.2005
+0.0.0.2 - 03.04.2005
+0.0.0.1 - 17.03.2005
+}
+unit AiIoFileKb;
+
+interface
+
+uses
+  Classes,
+  ABase, ABaseUtils3, AIoTypes, ATypes,
+  AiBaseUtils, AiIoFile, AiTypes;
+
+type
+  TFileProfKbHeader3 = packed record {32} {Заголовок файла БД фреймов}
+    CountF: UInt64;             {Колличество записей фреймов в файле}
+    Date: TDateTime64;          {Дата и время последнего использования (закрытия)}
+    //Version1: UInt32;           {=0}
+    //Version2: UInt32;           {=1}
+    Reserved1: UInt32;          {=0}
+    Reserved2: UInt32;          {=0}
+    {CountTableB: UInt032;       {Колличество блоков распределения свободного места}
+    {CountB: UInt032;            {Колличество блоков всего = CountF + CountTableB + Колличество дополнительных блоков}
+    {HBase: TAIFreimId;          {Идентификатор базового фрейма}
+  end;
+
+type
+  TFileProfKbHeader4 = packed record {32} {Заголовок файла БД фреймов}
+    CountF: UInt64;             {Колличество записей фреймов в файле}
+    Date: TDateTime64;          {Дата и время последнего использования (закрытия)}
+    Version1: UInt32;           {=0}
+    Version2: UInt32;           {=1}
+    Reserved1: UInt32;          {=0}
+    Reserved2: UInt32;          {=0}
+    {CountTableB: UInt032;       {Колличество блоков распределения свободного места}
+    {CountB: UInt032;            {Колличество блоков всего = CountF + CountTableB + Колличество дополнительных блоков}
+    {HBase: TAIFreimId;          {Идентификатор базового фрейма}
+  end;
+
+type
+  TFileProfKbHeader = TFileProfKbHeader4;
+
+type
+  TFileProfKB = class(TFileProf)
+  private
+    FHeaderKB: TFileProfKBHeader;
+    FHeaderKb3: TFileProfKbHeader3;
+    FPath: String;
+    function HeaderKBRead(var Header: TFileProfKBHeader): TError;
+    function HeaderKbRead3(var Header: TFileProfKbHeader3): TError;
+    function HeaderKBWrite(const Header: TFileProfKBHeader): TError;
+  public
+    constructor Create;
+    function FreimNew(Typ: TAI_Id): TAI_Id;
+    function FreimRead(Id: TAI_Id; var Rec: TAIFreimRec): TError;
+    function FreimRead64(Id: TAI_Id; var Rec: TAIFreimRecF64): TError;
+    function FreimWrite(Id: TAI_Id; const RecF: TAIFreimRec): Boolean;
+    function FreimWrite64(Id: TAI_Id; const RecF: TAIFreimRecF64): Boolean;
+    function FreimWrite64_2(Id: TAI_Id; const Value: TAiFreimRecF64): TError;
+    function FreimWriteNext(const Rec: TAIFreimRecF64): TError;
+    function GetHeaderKB: TFileProfKBHeader;
+    function GetPath: String;
+    function HeaderSet(var Header: TFileProfHeader): TError;
+    function Open(FileName: String; Path: String = ''): Boolean;
+    function Open2(const FileName, Path: APascalString): TError;
+    function OpenCreate(FileName: String; Path: String = ''): TError;
+  end;
+
+procedure ArrayByteToHeaderKB(
+  const A: TArrayByte;
+  var Header: TFileProfKBHeader
+  );
+
+(*procedure IOFileProfKBClose(
+  Handle: THandle
+  );
+
+function IOFileProfKBCreate(
+  FileName: String
+  ): THandle;*)
+
+procedure HeaderKBClear(
+  var Header: TFileProfKBHeader
+  );
+
+procedure HeaderKbClear3(var Header: TFileProfKbHeader3);
+
+implementation
+
+{Procedures and Functions}
+
+procedure ArrayByteToHeaderKB(const A: TArrayByte; var Header: TFileProfKBHeader);
+begin
+  Header.CountF := 0;
+  Header.Date := 0;
+  Header.Version1 := 0;
+  Header.Version2 := 0;
+  Header.Reserved1 := 0;
+  Header.Reserved2 := 0;
+end;
+
+(*procedure IOFileProfKBClose(Handle: THandle);
+begin
+  IOFileProfClose(Handle);
+end;
+
+function IOFileProfKBCreate(FileName: String): THandle;
+var
+  Header: TFileProfKBHeader;
+  HProf: TFileProfHeader;
+  Freim: TAIFreimRecF64;
+  I: Int32;
+const
+  CountFBase = 1024; {Колличество зарезервированых и базовых фреймов}
+begin
+  HProf.Prof := 'Prof';
+  HProf.Ident := 1;
+  HProf.Version1 := 2;
+  HProf.Version2 := 4;
+  Result := IOFileProfCreate(FileName, HProf);
+  if Result = 0 then Exit;
+  Header.CountF := CountFBase;
+  Header.Date := DateTimeNow;
+  if IOFileWrite(Result, Header, SizeOf(Header)) <> SizeOf(Header) then begin
+    IOFileProfClose(Result);
+    Result := 0;
+    Exit;
+  end;
+  Freim.Typ := 0;
+  Freim.DTCreate := DateTimeNow;
+  Freim.DataSize := 0;
+  Freim.ConnectCount := 0;
+  for I := 0 to CountFBase - 1 do begin
+    Freim.Id := I;
+    IOFileWrite(Result, Freim, SizeOf(Freim));
+  end;
+end;
+
+{function IOFileProfKBOpen(FileName: String; Mode: TFileOpenMode): THandle;
+begin
+  IOFileProfOpen
+end;}*)
+
+procedure HeaderKBClear(var Header: TFileProfKBHeader);
+begin
+  Header.CountF := 0;
+  Header.Date := 0;
+  Header.Version1 := 0;
+  Header.Version2 := 0;
+  Header.Reserved1 := 0;
+  Header.Reserved2 := 0;
+end;
+
+procedure HeaderKbClear3(var Header: TFileProfKbHeader3);
+begin
+  Header.CountF := 0;
+  Header.Date := 0;
+  //Header.Version1 := 0;
+  //Header.Version2 := 0;
+  Header.Reserved1 := 0;
+  Header.Reserved2 := 0;
+end;
+
+procedure HeaderToArrayByte(var A: TArrayByte; Header: TFileProfKBHeader);
+var
+  A1: TArrayByte;
+begin
+  SetLength(A, 32);
+  A1 := cUInt64ToArrayByte(Header.CountF);
+  ArrayByteInsert(A, A1, 0);
+  A1 := cFloat64ToArrayByte(Header.Date);
+  ArrayByteInsert(A, A1, 8);
+  A1 := cUInt32ToArrayByte(Header.Version1);
+  ArrayByteInsert(A, A1, 12);
+  A1 := cUInt32ToArrayByte(Header.Version2);
+  ArrayByteInsert(A, A1, 16);
+  A1 := cUInt32ToArrayByte(Header.Reserved1);
+  ArrayByteInsert(A, A1, 20);
+  A1 := cUInt32ToArrayByte(Header.Reserved2);
+  ArrayByteInsert(A, A1, 24);
+end;
+
+{TFileProfKB}
+
+constructor TFileProfKB.Create;
+begin
+  inherited Create;
+  FHeaderKB.CountF := 0;
+  FHeaderKB.Date := 0;
+  FHeaderKB.Version1 := 0;
+  FHeaderKB.Version2 := 0;
+  FHeaderKB.Reserved1 := 0;
+  FHeaderKB.Reserved2 := 0;
+end;
+
+function TFileProfKB.FreimNew(Typ: TAI_Id): TAI_Id;
+var
+  RecF: TAIFreimRecF64;
+begin
+  Result := 0;
+  RecF.Id := FHeaderKB.CountF;
+  RecF.Typ := Typ;
+  RecF.DTCreate := DateTimeNow;
+  RecF.DataSize := 0;
+  RecF.ConnectCount := 0;
+  if FreimWrite64(RecF.Id, RecF) then
+  begin
+    Result := RecF.Id;
+    Inc(FHeaderKB.CountF);
+    HeaderKBWrite(FHeaderKB);
+  end;
+end;
+
+function TFileProfKB.FreimRead(Id: TAI_Id; var Rec: TAIFreimRec): TError;
+var
+  {A: TArrayByte;}
+  {Stream: TFileStream;}
+  I64: UInt64;
+  Fl: Float64;
+begin
+  {Result := 1;}
+
+  {Stream := GetStream;
+  if not(Assigned(Stream)) then Exit;
+  Seek(SizeOf(TFileProfHeader) + SizeOf(TFileProfKBHeader) + Id * SizeOf(TAIFreimRecF64));
+  Stream.Read(RecF.Id, SizeOf(RecF.Id));
+  Stream.Read(RecF.Typ, SizeOf(RecF.Typ));
+  Stream.Read(RecF.DTCreate, SizeOf(RecF.DTCreate));
+  Stream.Read(RecF.DataSize, SizeOf(RecF.DataSize));
+  Stream.Read(RecF.ConnectCount, SizeOf(RecF.ConnectCount));}
+
+  ReadUInt64(I64); Rec.Id := I64;
+  ReadUInt64(I64); Rec.Typ := I64;
+  ReadFloat64(Fl); Rec.DTCreate := cFloat64ToDateTime64(Fl);
+  ReadUInt64(Rec.DataSize);
+  ReadUInt64(Rec.ConnectCount);
+
+  Result := 0;
+end;
+
+function TFileProfKB.FreimRead64(Id: TAI_Id; var Rec: TAIFreimRecF64): TError;
+var
+  {A: TArrayByte;}
+  {Stream: TFileStream;}
+  I64: UInt64;
+  Fl: Float64;
+begin
+  {Result := 1;}
+
+  {Stream := GetStream;
+  if not(Assigned(Stream)) then Exit;
+  Seek(SizeOf(TFileProfHeader) + SizeOf(TFileProfKBHeader) + Id * SizeOf(TAIFreimRecF64));
+  Stream.Read(RecF.Id, SizeOf(RecF.Id));
+  Stream.Read(RecF.Typ, SizeOf(RecF.Typ));
+  Stream.Read(RecF.DTCreate, SizeOf(RecF.DTCreate));
+  Stream.Read(RecF.DataSize, SizeOf(RecF.DataSize));
+  Stream.Read(RecF.ConnectCount, SizeOf(RecF.ConnectCount));}
+
+  ReadUInt64(I64); Rec.Id := I64;
+  ReadUInt64(I64); Rec.Typ := I64;
+  ReadFloat64(Fl); Rec.DTCreate := cFloat64ToDateTime64(Fl);
+  ReadUInt64(Rec.DataSize);
+  ReadUInt64(Rec.ConnectCount);
+
+  Result := 0;
+end;
+
+function TFileProfKB.FreimWrite(Id: TAI_Id; const RecF: TAIFreimRec): Boolean;
+var
+  A: TArrayByte;
+  Stream: TFileStream;
+begin
+  Result := False;
+  {Stream := GetStream;
+  if not(Assigned(Stream)) then Exit;
+  Seek(SizeOf(TFileProfHeader) + SizeOf(TFileProfKBHeader) + Id * SizeOf(TAIFreimRecF64));
+  Result := FreimWriteNext(RecF);}
+end;
+
+function TFileProfKB.FreimWrite64(Id: TAI_Id; const RecF: TAIFreimRecF64): Boolean;
+var
+  A: TArrayByte;
+  Stream: TFileStream;
+begin
+  Result := False;
+  {Stream := GetStream;
+  if not(Assigned(Stream)) then Exit;
+  Seek(SizeOf(TFileProfHeader) + SizeOf(TFileProfKBHeader) + Id * SizeOf(TAIFreimRecF64));
+  Result := FreimWriteNext(RecF);}
+end;
+
+function TFileProfKB.FreimWrite64_2(Id: TAI_Id; const Value: TAiFreimRecF64): TError;
+begin
+  if FreimWrite64(Id, Value) then
+    Result := 0
+  else
+    Result := -1;
+end;
+
+function TFileProfKB.FreimWriteNext(const Rec: TAIFreimRecF64): TError;
+var
+  Stream: TFileStream;
+begin
+  Result := 1;
+  {Stream := GetStream;
+  if not(Assigned(Stream)) then Exit;
+  Stream.Write(Rec.Id, SizeOf(Rec.Id));
+  Stream.Write(Rec.Typ, SizeOf(Rec.Typ));
+  Stream.Write(Rec.DTCreate, SizeOf(Rec.DTCreate));
+  Stream.Write(Rec.DataSize, SizeOf(Rec.DataSize));
+  Stream.Write(Rec.ConnectCount, SizeOf(Rec.ConnectCount));
+  Result := 0;}
+end;
+
+function TFileProfKB.GetHeaderKB: TFileProfKBHeader;
+begin
+  Result := FHeaderKB;
+end;
+
+function TFileProfKB.GetPath: String;
+begin
+  Result := FPath;
+end;
+
+function TFileProfKB.HeaderKBRead(var Header: TFileProfKBHeader): TError;
+begin
+  Result := 1;
+  HeaderKBClear(Header);
+  Self.Seek(SizeOf(TFileProfHeader));
+  Self.ReadUInt64(Header.CountF);
+  Self.ReadFloat64(Header.Date);
+  Self.ReadUInt32(Header.Version1);
+  Self.ReadUInt32(Header.Version2);
+  Self.ReadUInt32(Header.Reserved1);
+  Self.ReadUInt32(Header.Reserved2);
+  Result := 0;
+end;
+
+function TFileProfKB.HeaderKbRead3(var Header: TFileProfKbHeader3): TError;
+begin
+  Result := 1;
+  HeaderKbClear3(Header);
+  Self.Seek(SizeOf(TFileProfHeader));
+  Self.ReadUInt64(Header.CountF);
+  Self.ReadFloat64(Header.Date);
+  //Self.ReadUInt32(Header.Version1);
+  //Self.ReadUInt32(Header.Version2);
+  Self.ReadUInt32(Header.Reserved1);
+  Self.ReadUInt32(Header.Reserved2);
+  Result := 0;
+end;
+
+function TFileProfKB.HeaderKBWrite(const Header: TFileProfKBHeader): TError;
+var
+  Stream: TFileStream;
+begin
+  Result := 1;
+  {Stream := GetStream;
+  if not(Assigned(Stream)) then Exit;
+  Seek(SizeOf(TFileProfHeader));
+  Stream.Write(Header.CountF, SizeOf(Header.CountF));
+  Stream.Write(Header.Date, SizeOf(Header.Date));
+  Stream.Write(Header.Version1, SizeOf(Header.Version1));
+  Stream.Write(Header.Version2, SizeOf(Header.Version2));
+  Stream.Write(Header.Reserved1, SizeOf(Header.Reserved1));
+  Stream.Write(Header.Reserved2, SizeOf(Header.Reserved2));
+  Result := 0;}
+end;
+
+function TFileProfKB.HeaderSet(var Header: TFileProfHeader): TError;
+begin
+  Result := inherited HeaderSet(Header, 1, 2, 4);
+end;
+
+function TFileProfKB.Open(FileName: String; Path: String = ''): Boolean;
+begin
+  Result := inherited Open(FileName, Prof_fmOpenReadWrite);
+  if not(Result) then Exit;
+  Result := False;
+  if (Header.Ident <> 1) or (Header.Version1 <> 2) then
+  begin
+    Close;
+    Exit;
+  end;
+  if (Header.Version2 <> 3) and (Header.Version2 <> 4) then
+  begin
+    Close();
+    Exit;
+  end;
+
+  if (Header.Version2 = 3) then
+  begin
+    if (HeaderKbRead3(FHeaderKb3) <> 0) then
+    begin
+      Close();
+      Exit;
+    end;
+  end
+  else
+  begin
+    if (HeaderKbRead(FHeaderKb) <> 0) then
+    begin
+      Close();
+      Exit;
+    end;
+    if (FHeaderKb.Version1 <> 0) and (FHeaderKb.Version2 <> 1) then Exit;
+  end;
+  FPath := Path;
+  Result := True;
+end;
+
+function TFileProfKB.Open2(const FileName, Path: APascalString): TError;
+begin
+  if Open(FileName, Path) then
+    Result := 0
+  else
+    Result := -1;
+end;
+
+function TFileProfKB.OpenCreate(FileName: String; Path: String = ''): TError;
+var
+  I: Int32;
+  Header: TFileProfHeader;
+  Rec: TAIFreimRecF64;
+begin
+  FPath := Path;
+  {FHeaderKB := HeaderKB;}
+  FHeaderKB.CountF := 1024;
+  FHeaderKB.Date := DateTimeNow;
+  FHeaderKB.Version1 := 0;
+  FHeaderKB.Version2 := 1;
+  FHeaderKB.Reserved1 := 0;
+  FHeaderKB.Reserved2 := 0;
+  HeaderSet(Header);
+  {Создание файла Prof}
+  Result := inherited OpenCreate(FileName, Header);
+  if Result <> 0 then Exit;
+  {Запись заголовка KB}
+  if HeaderKBWrite(FHeaderKB) <> 0 then begin
+    Result := 1;
+    Close;
+    Exit;
+  end;
+  {Заполнение фреймами}
+  FreimRecF64Clear(Rec);
+  for I := 0 to FHeaderKB.CountF do begin
+    Rec.Id := I;
+    FreimWriteNext(Rec);
+  end;
+  {Rec.Id := 0; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 1; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 2; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 3; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 4; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 5; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 6; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 7; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 8; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 9; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 10; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 11; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 12; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 13; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 14; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 15; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 16; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 17; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 18; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 19; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 20; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 21; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 22; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 23; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 24; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 25; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 26; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 27; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 28; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 29; Rec.Typ := 0; FreimWriteNext(Rec);
+  Rec.Id := 30; Rec.Typ := 0; FreimWriteNext(Rec);}
+end;
+
+end.
