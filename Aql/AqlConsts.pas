cf6615214c8bd7f057d5a46a5fdb4f20b898504f
Prof1983 <prof1983@yandex.ru>
2012-06-01 10:25:25 +0600

    Added Aql from AReason/Ai

---
+++ Aql/AqlConsts.pas
@@ -0,0 +1,196 @@
+﻿{**
+@Abstract(Константы работы с языком AQL)
+@Author(Prof1983 prof1983@ya.ru)
+@Created(17.03.2007)
+@LastMod(03.05.2012)
+@Version(0.5)
+}
+unit AqlConsts;
+
+interface
+
+// Структура сообщений для обмена информацией между агентами:
+// <?xml version="1.0" encoding="E1"?>
+// <aql sender="X1" recepient="X2" lang="L1">
+// </aql>
+// Здесь:
+// E1 - Указывает в какой кодировке создано сообщение
+// X1 - Имя или ID объекта который отправил сообщение
+// X2 - Имя или ID объекта которому отправлено сообщения
+// L1 - Язык на котором составлено сообщение
+const
+  AQL           = 'aql';
+  AQL_SENDER    = 'sender';
+  AQL_RECEPIENT = 'recepient';
+  AQL_LANG      = 'lang';
+
+// Названия языков обмена информацией между агентами:
+const
+  AQL_LANG_UML    = 'uml';   // [Prof]
+  AQL_LANG_AR     = 'ar';    // [Prof]
+  AQL_LANG_XVRML  = 'xvrml'; // [Prof]
+  AQL_LANG_XFORM  = 'xform'; // [Prof]
+
+// -----------------------------------------------------------------------------
+// Константы для обмена сообщениями по fipa.org
+{
+http://fipa.org/specs/fipa00071/SC00071E.html
+FIPA ACL Message Representation in XML Specification
+}
+const
+  // Possible FIPA Communicative Acts. See [FIPA00037] for a full list of valid performatives.
+  communicative_acts = 'communicative-acts';
+    //accept-proposal
+    //agree
+    //cancel
+    //cfp
+    //confirm
+    //disconfirm
+    //failure
+    //inform
+    //not-understood
+    //propose
+    //query-if
+    //query-ref
+    //refuse
+    //reject-proposal
+    //request
+    //request-when
+    //request-whenever
+    //subscribe
+    //inform-if
+    //inform-ref
+    //proxy
+    //propagate
+
+const
+  {The FIPA message root element, the communicative act is 
+     an attribute - see below and the message itself is a list
+     of parameters. The list is unordered. None of the elements
+     should occur more than once except receiver.}
+  msg_param = 'msg-param';
+    //receiver
+    //sender
+    //content
+    //language
+    //encoding
+    //ontology
+    //protocol
+    //reply-with
+    //in-reply-to
+    //reply-by
+    //reply-to
+    //conversation-id
+    //user-defined
+
+// -----------------------------------------------------------------------------
+// Пример:
+{
+<fipa-message act="%act">
+  <sender>...agent-identifier...</sender>
+  <receiver>...agent-identifier...</receiver>
+  <content href="url">...</content>
+  <language href="url"></language>
+  <encoding href="url"></encoding>
+  <ontology href="url"></ontology>
+  <protocol href="url"></protocol>
+  <reply-with href="url"></reply-with>
+  <in-reply-to href="url"></in-reply-to>
+  <reply-by time="..." href="..."></reply-by>
+  <reply-to>AgentTo</reply-to>
+  <conversation-id href="url"></conversation-id>
+</fipa-message>
+
+<agent-identifier>
+  <name id="..." refid="...">agentName</name>
+  <addresses>
+    <url>...</url>
+    <url>...</url>
+  </addresses>
+  <resolvers>
+    <agent-identifier>
+      <name id="..." refid="...">agentName</name>
+      <addresses>
+        <url>...</url>
+        <url>...</url>
+      </addresses>
+      <resolvers>
+      </resolvers>
+      <user-defined>
+        <url>...</url>
+        <url>...</url>
+      </user-defined>
+    </agent-identifier>
+  </resolvers>
+  <user-defined>
+    <url>...</url>
+    <url>...</url>
+  </user-defined>
+</agent-identifier>
+
+
+name - An id can be used to uniquely identify the name of the agent.
+     The refid attribute can be used to refer to an already defined
+     agent name, avoiding unnecessary repetition. Either the id
+     OR refid should be specified, (both should not be present at the
+     same time).
+}
+
+// -----------------------------------------------------------------------------
+// http://fipa.org/specs/fipa00085/SC00085J.html
+{
+<!--
+Document Type: XML DTD
+Document Purpose: Encoding of FIPA ACL message envelopes (as in [FIPA0067]).
+See http://www.fipa.org
+Last Revised: 2000-08-16
+-->
+<!ELEMENT     envelope               ( params+ )>
+<!ELEMENT     params                 ( to?,
+                                      from?,
+                                      comments?,
+                                      acl-representation?,
+                                      payload-length?,
+                                      payload-encoding?,
+                                      date?,
+                                      encrypted?,
+                                      intended-receiver?,
+                                      received?,
+                                      user-defined* )>
+<!ATTLIST     params                  index CDATA #REQUIRED>
+<!ELEMENT     to                     ( agent-identifier+ )>
+<!ELEMENT     from                   ( agent-identifier )>
+<!ELEMENT     acl-representation     ( #PCDATA )>
+<!ELEMENT     comments               ( #PCDATA )>
+<!ELEMENT     payload-length         ( #PCDATA )>
+<!ELEMENT     payload-encoding       ( #PCDATA )>
+<!ELEMENT     date                   ( #PCDATA )>
+<!ELEMENT     intended-receiver      ( agent-identifier+ )>
+<!ELEMENT     agent-identifier       ( name,
+                                      addresses?,
+                                      resolvers?,
+                                      user-defined* )>
+<!ELEMENT     name                   ( #PCDATA )>
+<!ELEMENT     addresses              ( url+ )>
+<!ELEMENT     url                    ( #PCDATA )>
+<!ELEMENT     resolvers              ( agent-identifier+ )>
+<!ELEMENT     received               ( received-by,
+                                      received-from?,
+                                      received-date,
+                                      received-id?,
+                                      received-via?,
+                                      user-defined* )>
+<!ELEMENT     received-by            ( url )>
+<!ELEMENT     received-from          ( url )>
+<!ELEMENT     received-date           EMPTY>
+<!ATTLIST     received-date           value CDATA #IMPLIED>
+<!ELEMENT     received-id             EMPTY>
+<!ATTLIST     received-id             value CDATA #IMPLIED>
+<!ELEMENT     received-via            EMPTY>
+<!ATTLIST     received-via            value CDATA #IMPLIED>
+<!ELEMENT     user-defined           ( #PCDATA )>
+<!ATTLIST     user-defined            href CDATA #IMPLIED>
+}
+implementation
+
+end.
---
+++ Aql/AqlUtils.pas
@@ -0,0 +1,99 @@
+﻿{**
+@Abstract(Функции работы с языком AQL)
+@Author(Prof1983 prof1983@ya.ru)
+@Created(17.03.2007)
+@LastMod(03.05.2012)
+@Version(0.5)
+
+Формат сообщения на языке AQL.XML:
+<aql sender="X" recepient="Y" lang="Z">...</aql>
+
+Формат сообщения на языке AQL.String
+aql <Sender ID or Name> <Recepient ID or Name> <Lang ID or Name> <Message>
+
+Сообщения на языке AQL.Binnary пока не используются
+}
+unit AqlUtils;
+
+interface
+
+uses
+  AqlConsts;
+
+type
+  //** @abstract(Тип сообщения AQL)
+  TAqlMessageType = Integer;
+const
+  AQL_MESSAGE_NONE    = $01;
+  AQL_MESSAGE_UNKNOWN = $02;
+  AQL_MESSAGE_BINNARY = $04;
+  AQL_MESSAGE_STRING  = $08;
+  AQL_MESSAGE_XML     = $10;
+
+//** @abstract(Разкодировать AQL сообщение в виде строки)
+function AqlDecodeStr(const AStr: WideString; var ASender, ARecepient, ALang, ABody: WideString): WordBool;
+//** @abstract(Закодировать AQL сообщение в виде строки)
+function AqlEncodeStr(const ASender, ARecepient, ALang, ABody: WideString): WideString;
+//** @abstract(Закодировать AQL сообщение в виде XML строки)
+function AqlEncodeXml(const ASender, ARecepient, ALang, ABody: WideString): WideString;
+//** @abstract(Определяет тип сообщения AQL)
+function AqlMessageType(const AMsg: WideString): TAqlMessageType;
+
+implementation
+
+// -----------------------------------------------------------------------------
+function AqlDecodeStr(const AStr: WideString; var ASender, ARecepient, ALang, ABody: WideString): WordBool;
+var
+  i: Integer;
+  tmpStr: string;
+begin
+  tmpStr := AStr;
+  i := Pos(' ', tmpStr);
+  Result := (Copy(tmpStr, 1, i-1) = AQL);
+  if Result then
+  begin
+    Delete(tmpStr, 1, i);
+    i := Pos(' ', tmpStr);
+    ASender := Copy(tmpStr, 1, i-1);
+    Delete(tmpStr, 1, i);
+    i := Pos(' ', tmpStr);
+    ARecepient := Copy(tmpStr, 1, i-1);
+    Delete(tmpStr, 1, i);
+    i := Pos(' ', tmpStr);
+    ALang := Copy(tmpStr, 1, i-1);
+    Delete(tmpStr, 1, i);
+    ABody := tmpStr;
+  end;
+end;
+
+// -----------------------------------------------------------------------------
+function AqlEncodeStr(const ASender, ARecepient, ALang, ABody: WideString): WideString;
+begin
+  Result := AQL+' '+ASender+' '
+            +ARecepient+' '
+            +ALang+' '
+            +ABody;
+end;
+
+// -----------------------------------------------------------------------------
+function AqlEncodeXml(const ASender, ARecepient, ALang, ABody: WideString): WideString;
+begin
+  Result := '<'+AQL+' '+AQL_SENDER+'="'+ASender+'" '
+            +AQL_RECEPIENT+'="'+ARecepient+'" '
+            +AQL_LANG+'="'+ALang+'">'
+            +ABody
+            +'</'+AQL+'>';
+end;
+
+// -----------------------------------------------------------------------------
+function AqlMessageType(const AMsg: WideString): TAqlMessageType;
+begin
+  Result := AQL_MESSAGE_NONE;
+  if Length(AMsg) = 0 then Exit;
+  if AMsg[1] = '<' then
+    Result := AQL_MESSAGE_XML
+  else
+    Result := AQL_MESSAGE_STRING;
+end;
+
+end.
