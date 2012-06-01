4a829164c3476d680566cdca412e44874ea51910
Prof1983 <prof1983@yandex.ru>
2012-06-01 11:43:21 +0600

    Added EntityStorage from AReason/Ai

---
+++ EntityStorage/AiEntityStorageSettings.pas
@@ -0,0 +1,34 @@
+﻿{**
+@Abstract(Настройки программы EntityStorage)
+@Author(Prof1983 prof1983@ya.ru)
+@Created(06.07.2007)
+@LastMod(02.05.2012)
+@Version(0.5)
+
+История версий:
+0.0.0.1 - 29.06.2007 - Создал на основе Reminder.Settings
+}
+unit AiEntityStorageSettings;
+
+interface
+
+uses
+  AiBase, AiBaseTypes;
+
+type //** @abstract(Настройки программы EntityStorage)
+  TEntityStorageSettings = class
+  private
+      //** Идентификатор для заданий. Применяется в Pool.
+    FTaskTypeID: TAIID;
+      //** Заголовок окна
+    FTitle: WideString;
+  public // Секция "General"
+      //** Идентификатор для заданий. Применяется в Pool.
+    property TaskTypeID: TAIID read FTaskTypeID write FTaskTypeID;
+      //** Заголовок окна
+    property Title: WideString read FTitle write FTitle;
+  end;
+
+implementation
+
+end.
---
+++ EntityStorage/AiEntityStorageSettingsLoader.pas
@@ -0,0 +1,56 @@
+﻿{**
+@Abstract(Класс для загрузки параметров из INI файла)
+@Author(Prof1983 prof1983@ya.ru)
+@Created(06.07.2007)
+@LastMod(02.05.2012)
+@Version(0.5)
+
+История версий:
+0.0.0.1 - 06.07.2007 - Создал на основе Reminder.SettingsLoader
+}
+unit AiEntityStorageSettingsLoader;
+
+interface
+
+uses
+  IniFiles,
+  AiEntityStorageSettings;
+
+type //** @abstract(Класс для загрузки параметров из INI файла)
+  TEntityStorageSettingsLoader = class
+  public
+    class function Load(Settings: TEntityStorageSettings; FileName: WideString): Boolean;
+  end;
+
+implementation
+
+{ TEntityStorageSettingsLoader }
+
+class function TEntityStorageSettingsLoader.Load(Settings: TEntityStorageSettings; FileName: WideString): Boolean;
+var
+  f: TIniFile;
+begin
+  Result := False;
+  if not(Assigned(Settings)) then Exit;
+  if (FileName = '') then Exit;
+
+  Result := False;
+  try
+    f := TIniFile.Create(FileName);
+  except
+    Exit;
+  end;
+
+  try
+    Settings.Title := f.ReadString('General', 'Title', '');
+    Settings.TaskTypeID := f.ReadInteger('General', 'TaskTypeID', 0);
+  except
+    f.Free();
+    Exit;
+  end;
+
+  f.Free();
+  Result := True;
+end;
+
+end.
