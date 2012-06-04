4cad112151dbbb76768b199b4e604cc453c03064
Prof1983 <prof1983@yandex.ru>
2012-06-04 15:31:42 +0600

    Added TAiSourceFile20050915 type

--- Data/AiSourceFile.pas
+++ Data/AiSourceFile.pas
@@ -2,7 +2,7 @@
 @Abstract(Источник)
 @Author(Prof1983 prof1983@ya.ru)
 @Created(14.02.2006)
-@LastMod(18.05.2012)
+@LastMod(04.06.2012)
 @Version(0.5)
 
 0.0.0.1 - 14.02.2006 - From Prof_AI_Class
@@ -15,7 +15,7 @@ unit AiSourceFile;
 interface
 
 uses
-  ATypes,
+  ABase, ATypes,
   AiConnectsObj, AiDataObj, AiIoFileKb, AiTypes, AiSourceObj;
 
 type {258}
@@ -30,16 +30,16 @@ type {258}
     function GetFile: TFileProfKB;
     function GetFileName(): String;
     function GetFilePath(): String;
-    function GetFreimConnects(Id: TAI_Id): TAiConnectsObject; override;
-    function GetFreimData(Id: TAI_Id): TAiData; override; //function GetFreimData(Id: TAI_Id): TAI_Data; override;
-    function GetFreimDTCreate(Id: TAI_Id): TDateTime;
-    function GetFreimType(Id: TAI_Id): TAI_Id; override;
-    function NewFreim(Typ: TAI_Id; Id: TAI_Id = 0): TAI_Id; override;
-    function Open: Boolean; override;
+    function GetFreimConnects(Id: TAId): TAiConnectsObject; override;
+    function GetFreimData(Id: TAId): TAiDataObject; override;
+    function GetFreimDTCreate(Id: TAId): TDateTime;
+    function GetFreimType(Id: TAId): TAId; override;
+    function NewFreim(Typ: TAId; Id: TAId = 0): TAId; override;
+    function Open(): AError; override;
     function OpenCreate: Boolean;
     function SetFileName(const Value: string): Boolean;
     function SetFilePath(const Value: string): Boolean;
-    function SetFreimType(Id, Typ: TAI_Id): Boolean; override;
+    function SetFreimType(Id, Typ: TAId): Boolean; override;
   public
     constructor Create(const FileName, Path: string);
   end;
@@ -47,7 +47,27 @@ type {258}
   TAiSourceFile2004 = class(TAiSource2004)
   end;
 
-  {259}
+  TAiSourceFile20050915 = class(TAiSourceObject2005)
+  private
+    FF: TFileProfKB;
+    FFileName: String;
+    FFilePath: String;          {Путь для расположения дополнительных файлов}
+  public
+    function Close(): TError; override;
+    function GetCountFreims(): UInt64; override;
+    function GetFile(): TFileProfKB;
+    function GetFileName(): String;
+    function GetFilePath(): String;
+    function GetFreimDTCreate(Id: TAId): TDateTime;
+    function GetFreimType(Id: TAId): TAId; override;
+    function NewFreim(Typ: TAId; Id: TAId = 0): TAId; override;
+    function Open(): TError; override;
+    function OpenCreate(): AError;
+    function SetFileName(Value: String): AError;
+    function SetFilePath(Value: String): AError;
+    function SetFreimType(Id, Typ: TAId): AError; override;
+  end;
+
   TAISourceFileCashe = TAISourceFile;
 
 implementation
@@ -91,12 +111,12 @@ begin
   Result := FFilePath;
 end;
 
-function TAISourceFile.GetFreimConnects(Id: TAI_Id): TAiConnectsObject;
+function TAISourceFile.GetFreimConnects(Id: TAId): TAiConnectsObject;
 begin
   Result := nil;
 end;
 
-function TAISourceFile.GetFreimData(Id: TAI_Id): TAiData;
+function TAISourceFile.GetFreimData(Id: TAId): TAiDataObject;
 begin
   {Result := inherited FreimDataGet(Id);
   Result := nil;
@@ -104,7 +124,7 @@ begin
   Result := nil;
 end;
 
-function TAISourceFile.GetFreimDTCreate(Id: TAI_Id): TDateTime;
+function TAISourceFile.GetFreimDTCreate(Id: TAId): TDateTime;
 var
   Rec: TAIFreimRecF64;
 begin
@@ -116,7 +136,7 @@ begin
   Result := Rec.DTCreate;
 end;
 
-function TAISourceFile.GetFreimType(Id: TAI_Id): TAI_Id;
+function TAISourceFile.GetFreimType(Id: TAId): TAId;
 var
   Rec: TAIFreimRecF64;
 begin
@@ -128,23 +148,26 @@ begin
   Result := Rec.Typ;
 end;
 
-function TAISourceFile.NewFreim(Typ: TAI_Id; Id: TAI_Id = 0): TAI_Id;
+function TAISourceFile.NewFreim(Typ: TAId; Id: TAId = 0): TAId;
 begin
   Result := 0;
   if not(Assigned(FF)) then Exit;
   Result := FF.FreimNew(Typ);
 end;
 
-function TAISourceFile.Open(): Boolean;
+function TAISourceFile.Open(): AError;
 begin
   Close();
   if (FFileName = '') or (FFilePath = '') then
   begin
-    Result := False;
+    Result := -2;
     Exit;
   end;
   FF := TFileProfKB.Create();
-  Result := FF.Open(FFileName);
+  if FF.Open(FFileName) then
+    Result := 0
+  else
+    Result := -1;
 end;
 
 function TAISourceFile.OpenCreate: Boolean;
@@ -152,7 +175,7 @@ begin
   Close();
   Result := SaveSourceToFileN(Self, FFileName, FFilePath);
   if not(Result) then Exit;
-  Result := Open();
+  Result := (Open() >= 0);
 end;
 
 function TAISourceFile.SetFileName(const Value: string): Boolean;
@@ -177,7 +200,7 @@ begin
   Result := True;
 end;
 
-function TAISourceFile.SetFreimType(Id, Typ: TAI_Id): Boolean;
+function TAISourceFile.SetFreimType(Id, Typ: TAId): Boolean;
 var
   Rec: TAIFreimRecF64;
 begin
@@ -188,6 +211,114 @@ begin
   if FF.FreimRead64(Id, Rec) <> 0 then Exit;
   Rec.Typ := Typ;
   Result := FF.FreimWrite64(Id, Rec);
+end;
+
+{ TAiSourceFile20050915 }
+
+function TAiSourceFile20050915.Close(): AError;
+begin
+  if Assigned(FF) then FF.Free;
+  FF := nil;
+  Result := inherited Close;
+end;
+
+function TAiSourceFile20050915.GetCountFreims(): UInt64;
+begin
+  Result := 0;
+  if not(Assigned(FF)) then Exit;
+  Result := FF.GetHeaderKB.CountF;
+end;
+
+function TAiSourceFile20050915.GetFile(): TFileProfKB;
+begin
+  Result := FF;
+end;
+
+function TAiSourceFile20050915.GetFileName(): String;
+begin
+
+end;
+
+function TAiSourceFile20050915.GetFilePath(): String;
+begin
+  Result := FFilePath;
+end;
+
+function TAiSourceFile20050915.GetFreimDTCreate(Id: TAId): TDateTime;
+var
+  Rec: TAiFreimRecF64;
+begin
+  Result := 0;
+  if not(Assigned(FF)) then Exit;
+  {Result := FF.DTCreate(Id);}
+  if Id >= GetCountFreims then Exit;
+  if FF.FreimRead64(Id, Rec) <> 0 then Exit;
+  Result := Rec.DTCreate;
+end;
+
+function TAiSourceFile20050915.GetFreimType(Id: TAId): TAId;
+var
+  Rec: TAIFreimRecF64;
+begin
+  Result := 0;
+  if not(Assigned(FF)) then Exit;
+  {Result := FF.TypeGet(Id);}
+  if Id >= GetCountFreims then Exit;
+  if FF.FreimRead64(Id, Rec) <> 0 then Exit;
+  Result := Rec.Typ;
+end;
+
+function TAiSourceFile20050915.NewFreim(Typ: TAId; Id: TAId = 0): TAId;
+begin
+  Result := 0;
+  if not(Assigned(FF)) then Exit;
+  Result := FF.FreimNew(Typ);
+end;
+
+function TAiSourceFile20050915.Open(): AError;
+begin
+  Close;
+  if (FFileName = '') or (FFilePath = '') then
+  begin
+    Result := -1;
+    Exit;
+  end;
+  FF := TFileProfKB.Create;
+  Result := FF.Open2(FFileName, '');
+end;
+
+function TAiSourceFile20050915.OpenCreate(): AError;
+begin
+  Close;
+  if SaveSourceToFileN(Self, FFileName, FFilePath) then
+    Result := 0
+  else
+    Result := -1;
+  if Result <> 0 then Exit;
+  Result := Open;
+end;
+
+function TAiSourceFile20050915.SetFileName(Value: String): AError;
+begin
+  Result := 1;
+end;
+
+function TAiSourceFile20050915.SetFilePath(Value: String): AError;
+begin
+  Result := 1;
+end;
+
+function TAiSourceFile20050915.SetFreimType(Id, Typ: TAId): AError;
+var
+  Rec: TAIFreimRecF64;
+begin
+  Result := 1;
+  if not(Assigned(FF)) then Exit;
+  {Result := FF.TypeSet(Id, Typ);}
+  if Id >= GetCountFreims then Exit;
+  if FF.FreimRead64(Id, Rec) <> 0 then Exit;
+  Rec.Typ := Typ;
+  Result := FF.FreimWrite64_2(Id, Rec);
 end;
 
 end.
