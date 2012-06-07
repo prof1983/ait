9cb3644a3566772034793a4b5cb1ee8a3f8e7616
Prof1983 <prof1983@yandex.ru>
2012-06-07 11:32:26 +0600

    Added UAISource.pas

---
+++ Units/UAISource.pas
@@ -0,0 +1,27 @@
+{**
+@Abstract(UAISource recovered)
+@Author(Prof1983 prof1983@ya.ru)
+@Created(<25.08.2004)
+@LastMod(07.06.2012)
+@Version(0.0)
+}
+unit UAISource;
+
+interface
+
+procedure AiSource_Close();
+procedure AiSource_Open(const FileName: String; Num: Integer);
+
+implementation
+
+procedure AiSource_Close();
+begin
+  // TODO: Build
+end;
+
+procedure AiSource_Open(const FileName: String; Num: Integer);
+begin
+  // TODO: Build
+end;
+
+end.
