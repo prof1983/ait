d187bceba5a4c12677d8f076006e67b97ea5e851
Prof1983 <prof1983@yandex.ru>
2012-08-09 10:23:17 +0600

    Updated AiKernel client

--- Common/AiKernel.pas
+++ Common/AiKernel.pas
@@ -1,20 +1,21 @@
 {**
-@Abstract(AiKernel lib client)
-@Author(Prof1983 prof1983@ya.ru)
-@Created(19.06.2012)
-@LastMod(19.06.2012)
-@Version(0.0)
+@Abstract AiKernel lib client
+@Author Prof1983 <prof1983@ya.ru>
+@Created 19.06.2012
+@LastMod 09.08.2012
 }
 unit AiKernel;
 
 interface
 
 uses
-  ABase;
+  ABase, ALibraries;
+
+{** Boot AiKernel functions from library }
+function AiKernel_Boot(): AError;
 
 {** Initializes AiKernel
- *  @return AError value
- *}
+    @return AError value }
 function AiKernel_Init(): AError; cdecl;
 
 implementation
@@ -22,6 +23,46 @@ implementation
 const
   AiKernelLib = 'AiKernel.dll';
 
-function AiKernel_Init(): AError; cdecl; external AiKernelLib;
+type
+  AiKernel_Init_Proc = function(): AError; cdecl;
+
+var
+  IsBooted: Boolean;
+  _AiKernel_Init: AiKernel_Init_Proc;
+
+// --- External ---
+
+//function AiKernel_Init(): AError; cdecl; external AiKernelLib;
+
+// --- AiKernel ---
+
+function AiKernel_Boot(): AError;
+var
+  Lib: ALibrary;
+begin
+  Lib := Library_Open(AiKernelLib, 0);
+  if (Lib = 0) then
+  begin
+    Result := -1;
+    Exit;
+  end;
+  _AiKernel_Init := Library_GetProcAddress(Lib, 'AiKernel_Init');
+end;
+
+function AiKernel_Init(): AError;
+begin
+  if not(IsBooted) then
+    AiKernel_Boot();
+  if not(Assigned(_AiKernel_Init)) then
+  begin
+    Result := -1;
+    Exit;
+  end;
+  try
+    Result := _AiKernel_Init()
+  except
+    Result := -1;
+  end;
+end;
 
 end.
