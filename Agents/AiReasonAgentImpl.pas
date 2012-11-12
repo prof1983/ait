0b99d961e1d402ed3342cfcbb03b5c5c07dcbf21
Prof1983 <prof1983@yandex.ru>
2012-11-12 10:00:05 +0600

    Updated AiReasonAgentImpl

--- Agents/AiReasonAgentImpl.pas
+++ Agents/AiReasonAgentImpl.pas
@@ -1,9 +1,8 @@
 ﻿{**
-@Abstract(Главный Агент реализации разума)
-@Author(Prof1983 prof1983@ya.ru)
-@Created(30.10.2006)
-@LastMod(28.06.2012)
-@Version(0.5)
+@Abstract Главный Агент реализации разума
+@Author Prof1983 <prof1983@ya.ru>
+@Created 30.10.2006
+@LastMod 12.11.2012
 }
 unit AiReasonAgentImpl;
 
@@ -15,13 +14,16 @@ uses
   ABase,
   AiAgentImpl, AiGlobalTaskListImpl, AiReasonProcess, AiWorldImpl;
 
+type
+  TProcReasonProgress = procedure(AID, AProgress: Integer) of object;
+
 type //** Главный Агент реализации разума
   TAiReasonAgent = class(TAiAgent)
-  private
+  protected
       //** Глобальные задачи. Задаются один раз при создании и больше не меняются.
     FGlobalTasks: TAiGlobalTaskList;
       //** Главный объект отражения внешнего мира
-    FWorld: TAIWorld3;
+    FWorld: TAiWorld;
     // ... // Предметные области для решения локальных задач
       //** Сессия
     //FSession: TARSession;
@@ -37,41 +39,12 @@ type //** Главный Агент реализации разума
       //** Глобальные задачи. Задаются один раз при создании и больше не меняются.
     property GlobalTasks: TAiGlobalTaskList read FGlobalTasks;
       //** Главный объект отражения внешнего мира
-    property World: TAIWorld3 read FWorld;
+    property World: TAiWorld read FWorld;
   public // События
     //property OnTaskAdd: TProcMessage read FOnTaskAdd write FOnTaskAdd;
   end;
 
-type
-  TProcReasonProgress = procedure(AID, AProgress: Integer) of object;
-
-type //** Главный Агент реализации разума
-  TAiReasonAgent2007 = class(TAiAgent)
-  private
-    //** Главный объект отражения внешнего мира
-    FWorld: TAIWorld;
-    // ... // Предметные области для решения локальных задач
-    // Глобальные задачи. Задаются один раз при создании и больше не меняются.
-    //FGlobalTasks: TAIGlobalTaskList;
-    //** Сессия
-    //FSession: TARSession;
-    // ... // Предметные области для решения локальных задач
-    // ... // Глобальные задачи. Задаются один раз при создании и больше не меняются.
-  private // События
-    //FOnProgress: TProcReasonProgress;
-    //FOnTaskAdd: TProcMessage;
-  protected
-    function DoStart(): WordBool; override; safecall;
-  public
-    function Finalize(): AError; override;
-    function Initialize(): AError; override;
-  public
-    //property GlobalTasks: TAIGlobalTaskList read FGlobalTasks;
-    property World: TAIWorld read FWorld;
-  public // События
-    //property OnProgress: TProcReasonProgress read FOnProgress write FOnProgress;
-    //property OnTaskAdd: TProcMessage read FOnTaskAdd write FOnTaskAdd;
-  end;
+  //TAiReasonAgent2007 = TAiReasonAgent;
 
 implementation
 
@@ -120,7 +93,7 @@ begin
   // Создаем главный объект отражения внешного мира
   if not(Assigned(FWorld)) then
   begin
-    FWorld := TAIWorld3.Create();
+    FWorld := TAiWorld.Create();
     FWorld.Pool := FPool;
     FWorld.Initialize();
   end;
@@ -162,95 +135,6 @@ begin
 
   // Создаем предметные области решения локальных задач
   // ...
-end;
-
-function TAiReasonAgent.DoStart(): WordBool;
-begin
-  //Result := inherited DoStart();
-  Result := True;
-
-  // Запуск работы локального разума
-  //FThread.Resume();
-end;
-
-{ TAiReasonAgent2007 }
-
-function TAiReasonAgent2007.Finalize(): AError;
-begin
-  {try
-    FGlobalTasks.Free();
-    FGlobalTasks := nil;
-  except
-    FGlobalTasks := nil;
-  end;}
-
-  if Assigned(FWorld) then
-  try
-    FWorld.Free();
-    FWorld := nil;
-  except
-  end;
-
-  Result := inherited Finalize();
-end;
-
-function TAiReasonAgent2007.Initialize(): AError;
-begin
-  Result := inherited Initialize();
-
-  // Создание процесса работы локального разума
-  //FThread := TAIReasonThread.Create(False);
-
-  {// Создаем БЗ
-  if not(Assigned(FSource)) then
-  begin
-    FSource := TAISource.Create();
-  end;}
-
-  // Создаем главный объект отражения внешного мира
-  if not(Assigned(FWorld)) then
-  begin
-    FWorld := TAIWorld.Create();
-    //FWorld.Source := FSource;
-    FWorld.Initialize();
-  end;
-
-  {// Создаем внутреннея Я
-  if not(Assigned(FInternalI)) then
-  begin
-    FInternalI := TAIInternalI.Create();
-  end;}
-
-  // Создаем предметные области решения локальных задач
-  // ...
-
-  {// Создаем глобальные задачи
-  if not(Assigned(FGlobalTasks)) then
-  begin
-    FGlobalTasks := TAIGlobalTaskList.Create();
-    FGlobalTasks.Source := FSource;
-    FGlobalTasks.Initialize();
-  end;}
-
-  {// Создаем интерпретатор кода на языке AR
-  if not(Assigned(FInterpretator)) then
-  try
-    FInterpretator := TAICodeInterpretator.Create();
-    FInterpretator.Initialize();
-  except
-    FInterpretator := nil;
-  end;}
-
-  {AddToLog(lgNone, ltInformation, 'Создаем новую сессию');
-  FSession := TARSession.Create();
-  // ...
-  Result := True;}
-
-  {AddToLog(lgNone, ltInformation, 'Создаем главный объект отражения внешного мира');
-  FWorld := TARWorld.Create();}
-
-  // Создаем предметные области решения локальных задач
-  // ...
 
   // Создаем глобальные задачи
   // ...
@@ -262,10 +146,9 @@ begin
   // - Узнать что за пределами вселенной.
   // - Узнать что за пределами времени: где начало и где конец.
   // - Просто жить.
-
 end;
 
-function TAiReasonAgent2007.DoStart(): WordBool;
+function TAiReasonAgent.DoStart(): WordBool;
 begin
   //Result := inherited DoStart();
   Result := True;
